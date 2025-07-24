2445424d3c2c6464ba837112bea76e2e14dece79
SanisharaRamadhani <sanishara.ramadhani@gmail.com>
2025-07-24 20:53:03 +0700

    fix: escape apostrophe for react/no-unescaped-entities

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -23,16 +23,26 @@ const Products = () => {
   };
 
   const Carousel = ({ isMobile }: { isMobile: boolean }) => (
-    <div className={`justify-center ${isMobile ? 'inline-flex lg:hidden' : 'hidden lg:inline-flex'}`}>
+    <div
+      className={`justify-center ${
+        isMobile ? 'inline-flex lg:hidden' : 'hidden lg:inline-flex'
+      }`}
+    >
       <div
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
         className="min-w-[300px] min-h-[300px] rounded-2xl bg-center bg-cover duration-500"
       />
       <div className="absolute mt-[250px] flex justify-between w-full px-5">
-        <div onClick={prevSlide} className="rounded-full p-1 bg-black/20 text-white cursor-pointer">
+        <div
+          onClick={prevSlide}
+          className="rounded-full p-1 bg-black/20 text-white cursor-pointer"
+        >
           <BsChevronCompactLeft />
         </div>
-        <div onClick={nextSlide} className="rounded-full p-1 bg-black/20 text-white cursor-pointer">
+        <div
+          onClick={nextSlide}
+          className="rounded-full p-1 bg-black/20 text-white cursor-pointer"
+        >
           <BsChevronCompactRight />
         </div>
       </div>
@@ -49,7 +59,10 @@ const Products = () => {
         <div className="xs:mt-10 lg:mt-20 font-semibold">SELECT SIZE</div>
         <div className="flex flex-row my-10">
           {['S', 'M', 'L'].map((size) => (
-            <button key={size} className="mx-5 border-2 px-4 py-1 text-xs font-bold">
+            <button
+              key={size}
+              className="mx-5 border-2 px-4 py-1 text-xs font-bold"
+            >
               {size}
             </button>
           ))}
@@ -57,20 +70,29 @@ const Products = () => {
 
         <div className="font-semibold">SELECT COLOR</div>
         <div className="flex flex-row my-10">
-          {['red-600', 'green-600', 'blue-600', 'yellow-300'].map((color, idx) => (
-            <button
-              key={idx}
-              className={`mx-3 p-3 border-2 rounded-full bg-${color}`}
-              style={{ borderColor: color.replace('-600', '-800') }}
-            />
-          ))}
+          {['red-600', 'green-600', 'blue-600', 'yellow-300'].map(
+            (color, idx) => (
+              <button
+                key={idx}
+                className={`mx-3 p-3 border-2 rounded-full bg-${color}`}
+                style={{ borderColor: color.replace('-600', '-800') }}
+              />
+            )
+          )}
         </div>
 
         <div className="flex flex-col">
-          <div className="mt-5 flex justify-center bg-black text-white py-2">Price</div>
+          <div className="mt-5 flex justify-center bg-black text-white py-2">
+            Price
+          </div>
           <div className="my-5 flex justify-center font-bold">Rp. 100.000</div>
           <div className="flex flex-row justify-center items-center">
-            <input type="number" defaultValue={1} min={1} className="border-2 px-2 w-[70px] text-center" />
+            <input
+              type="number"
+              defaultValue={1}
+              min={1}
+              className="border-2 px-2 w-[70px] text-center"
+            />
             <span className="ml-2">pcs</span>
           </div>
           <button className="mb-10 mt-5 border-2 px-4 py-2 rounded-md text-white bg-black">
@@ -89,10 +111,11 @@ const Products = () => {
         <h1 className="font-semibold xs:mt-5 lg:mt-10">POT GLAZE</h1>
         <h1 className="font-bold xs:my-5 lg:my-10">CHINESE ORNAMENT</h1>
         <p className="font-light text-justify">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. It has been the
-          industry's standard dummy text ever since the 1500s. When an unknown printer took a galley of
-          type and scrambled it to make a type specimen book, it survived five centuries and transitioned
-          into electronic typesetting.
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. It has been the industry&apos;s standard dummy text ever
+          since the 1500s. When an unknown printer took a galley of type and
+          scrambled it to make a type specimen book, it survived five centuries
+          and transitioned into electronic typesetting.
         </p>
       </div>
     </div>
